4a2ad5c695fa239891e5c2ff18996a4513549c58
inhachoi <inhachoi@users.noreply.github.com>
2026-07-20 13:19:40 +0000

    deploy: 05794c806a52377f6713d94bef8ff178bde2e83d

---
+++ assets/formatter-EIJCOSYU-CMvh2Y8V.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-CCy06Jah.js";import{g as t}from"./iframe-BqAFRsp-.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-Cm7D96ty.js";import"./index-DwY_aqdO.js";import"./index-CbHImUhp.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
