be52b5eacbd8314da2fb6ca0b1415d38b7424ecc
2YH02 <2YH02@users.noreply.github.com>
2026-08-27 10:19:36 +0000

    deploy: 33cbc157c7ce195d53c2611dfb8a832c7c72233f

---
+++ assets/formatter-EIJCOSYU-ClviMCX2.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-cVzo4kju.js";import{g as t}from"./iframe-c9-ccGVG.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-DqmnSTq7.js";import"./index-CPPqcCop.js";import"./index-BOwaNtDz.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
---
+++ assets/index-BOwaNtDz.js
@@ -0,0 +1,1 @@
+import{t as r}from"./iframe-c9-ccGVG.js";import{r as t}from"./index-CPPqcCop.js";var o=t();const m=r(o);export{m as R,o as r};
