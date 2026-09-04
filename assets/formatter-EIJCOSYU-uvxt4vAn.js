71af098120697edc00789e01d7af6810360009ee
2YH02 <2YH02@users.noreply.github.com>
2026-09-04 04:58:28 +0000

    deploy: 86fde7f36dd459f3b386e51c1afd6201da68975a

---
+++ assets/formatter-EIJCOSYU-uvxt4vAn.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-lGl9k5lG.js";import{g as t}from"./iframe-BNypdzs7.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-C0BNJf9V.js";import"./index-BQtUlwJj.js";import"./index-DrouXQa8.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
